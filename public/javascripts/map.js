86a3fe1c1fcc24df3341541c5d68d69eeb2fd1ea
Juan <juanftp100@gmail.com>
2020-11-22 22:05:29 -0300

    api de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-L.marker([-25.34427, -57.58101]).addTo(mymap)
-    .bindPopup('Mi casita')
-    .openPopup();
+axios.get('/api/bicicletas').then(({data: {bicicletas}}) => {
+    bicicletas && bicicletas.map(item => {
+        L.marker([item.ubicacion[0], item.ubicacion[1]], {title: item.id}).addTo(mymap)
+        .bindPopup(`Bicicleta color: ${item.color}, modelo: ${item.modelo}`)
+        .openPopup();
+    })
+})
